b1fbdff5ea217aa3c76f9e188b6985acfd9b7f77
cxnt <pavelbartsits@gmail.com>
2021-07-01 17:49:34 +0300

    add modal window and object tags support

--- src/main.py
+++ src/main.py
@@ -11,26 +11,37 @@ WORKSPACE_ID = int(os.environ['context.workspaceId'])
 PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
 TASK_ID = int(os.environ["TASK_ID"])
 
+MODE = os.environ['modal.state.saveMode']
+
 
 @my_app.callback("tags_to_images_urls")
 @sly.timeit
 def tags_to_images_urls(api: sly.Api, task_id, context, state, app_logger):
-
     tags_to_urls = {}
     project_name = api.project.get_info_by_id(PROJECT_ID).name
     file_remote = "/tags_to_urls/{}_{}_{}.json".format(TASK_ID, TEAM_ID, project_name)
     meta_json = api.project.get_meta(PROJECT_ID)
     meta = sly.ProjectMeta.from_json(meta_json)
+
     for tag_meta in meta.tag_metas:
         tags_to_urls[tag_meta.name] = []
     id_to_tag_meta = meta.tag_metas.get_id_mapping()
     datasets = api.dataset.get_list(PROJECT_ID)
     for dataset in datasets:
-        images = api.image.get_list(dataset.id)
-        for image_info in images:
-            tags = TagCollection.from_api_response(image_info.tags, meta.tag_metas, id_to_tag_meta)
-            for tag in tags:
-                tags_to_urls[tag.name].append(image_info.full_storage_url)
+        if MODE == "both" or MODE == "Only images":
+            images = api.image.get_list(dataset.id)
+            for image_info in images:
+                img_tags = TagCollection.from_api_response(image_info.tags, meta.tag_metas, id_to_tag_meta)
+                for img_tag in img_tags:
+                    tags_to_urls[img_tag.name].append(image_info.full_storage_url)
+        if MODE == "both" or MODE == "Only objects":
+            anns = api.annotation.get_list(dataset.id)
+            for ann_info in anns:
+                ann = sly.Annotation.from_json(ann_info.annotation, meta)
+                image_info = api.image.get_info_by_id(ann_info.image_id)
+                for label in ann.labels:
+                    for lbl_tag in label.tags:
+                        tags_to_urls[lbl_tag.name].append(image_info.full_storage_url)
 
     file_local = os.path.join(my_app.data_dir, file_remote.lstrip("/"))
     app_logger.info("Local file path: {!r}".format(file_local))
